30c1406412cf790f0e5722e39ad2723b4839d0b9
sharan sangolagi <120734667+sharansangolagi1010@users.noreply.github.com>
2023-11-04 18:26:18 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import json
 from PIL import Image
 
 
-image=Image.open(r"C:\Users\hp\Downloads\cars projest with deployment\price_predition\image.jpg")
+image=Image.open(r"C:\Users\hp\Downloads\cars projest with deployment\image.jpg")
 st.image(image,caption="car price predicition")
 
 st.title("CAR PRICE PREDICITION")
